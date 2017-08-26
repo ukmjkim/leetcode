457635091a4515d34ea77c47c78574b2d3968bd3
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-08-25 18:25:03 -0700

    shortest word ladder

--- problem127_ladderLength/Solution.java
+++ problem127_ladderLength/Solution.java
@@ -2,42 +2,90 @@ import java.util.*;
 
 public class Solution {
 	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-		if (beginWord.equals(endWord)) return 1;
-		Set<String> set = new HashSet<>(wordList);
-		Queue<String> q = new LinkedList<>();
+		Set<String> wordSet = new HashSet<>(wordList);
 
-		int len = 1;
-		q.offer(beginWord);
-		while(!q.isEmpty()) {
-			int size = q.size();
-			for (int i = 0; i < size; i++) {
-				String curr = q.poll();
-				for (int j=0; j < curr.length(); j++) {
-					StringBuilder sb = new StringBuilder(curr);
-					for (char c = 'a'; c <= 'z'; c++) {
-						sb.setCharAt(j, c);
-						String tmp = sb.toString();
-						if (set.contains(tmp)) {
-							len ++;
-							if (tmp.equals(endWord)) return len;
-							q.offer(tmp);
-							set.remove(tmp);
-							j = curr.length();
-							break;
-						}
-					}
-				}
-			}
+		wordSet.add(beginWord);
+		List<List<String>> res = new ArrayList<List<String>>();
+		if (!wordSet.contains(endWord)) {
+			return 1;
 		}
-		return 0;	
+
+		List<String> path = new ArrayList<String>();
+		Map<String, List<String>> graph = new HashMap<String, List<String>>();
+		Map<String, Integer> dist = new HashMap<>();
+
+		bfs(graph, dist, beginWord, endWord, wordSet);
+
+		int shortestDepth = 0;
+		for (String str : dist.keySet()) {
+			shortestDepth = Math.max(shortestDepth, dist.get(str));
+		}
+
+		return shortestDepth + 1;
 	}
+
+	public static void bfs(Map<String, List<String>> graph, Map<String, 
+                    Integer> dist, String beginWord, 
+                    String endWord, Set<String> wordSet) {
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        dist.put(beginWord, 0);
+
+        for (String w: wordSet) {
+            graph.put(w, new ArrayList<String>());
+        }
+
+        while (!queue.isEmpty()) {
+            String word = queue.poll();
+            List<String> neighbors = getNeighbors(word, wordSet);
+            for (String neighbor: neighbors) {
+                graph.get(neighbor).add(word);
+                if (!dist.containsKey(neighbor)) {
+                    dist.put(neighbor, dist.get(word) + 1);
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+
+		for (String str : graph.keySet()) {
+			System.out.println(str + ": " + graph.get(str));
+		}
+
+        System.out.println();
+
+		for (String str : dist.keySet()) {
+			System.out.println(str + ": " + dist.get(str));
+		}
+
+    }
+
+	static List<String> getNeighbors(String word, Set<String> wordSet) {
+        List<String> res = new ArrayList<String>();
+        for (int i = 0; i < word.length(); i++) {
+            for (char ch = 'a'; ch < 'z' + 1; ch++) {
+                char[] chs = word.toCharArray();
+                if (ch != chs[i]) {
+                    chs[i] = ch;
+                    String next = new String(chs);
+                    //System.out.println(next);
+                    if (wordSet.contains(next)) {
+                        res.add(next);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
 
 	public static void main(String[] args) {
 		String beginWord = "hit";
 		String endWord = "cog";
 		List<String> wordList = new ArrayList<String>(Arrays.asList("hot", "dot", "aot", "dog", "lot", "log", "cog"));
 		Solution solution = new Solution();
-		int result = solution.ladderLength(beginWord, endWord, wordList);
-		System.out.println("The length of ladder worlds: " + result);
+		int ladderLength = solution.ladderLength(beginWord, endWord, wordList);
+		System.out.println("Shortest Depth: " + ladderLength);
 	}
 }

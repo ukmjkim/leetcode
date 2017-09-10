60e5583f7d5f5538da23597d4c77d0ba4a61829c
Mijung Kim <MijungKimMacPro@Mijungs-MacBook-Pro-2.local>
2017-09-09 20:57:30 -0700

    added iterative logic

--- problem617_mergeTwoBT/Solution.java
+++ problem617_mergeTwoBT/Solution.java
@@ -18,6 +18,35 @@ public class Solution {
     return newNode;
   }
 
+  public TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
+    if (t1 == null) {
+      return t2;
+    }
+    
+    Stack<TreeNode[]> stack = new Stack<>();
+    stack.push(new TreeNode[] {t1, t2});
+    while (!stack.isEmpty()) {
+      TreeNode[] t = stack.pop();
+      if (t[0] == null || t[1] == null) {
+        continue;
+      }
+      t[0].val += t[1].val;
+      if (t[0].left == null) {
+        t[0].left = t[1].left;
+      } else {
+        stack.push(new TreeNode[] {t[0].left, t[1].left});
+      }
+
+      if (t[0].right == null) {
+        t[0].right = t[1].right;
+      } else {
+        stack.push(new TreeNode[] {t[0].right, t[1].right});
+      }
+    }
+
+    return t1;
+  }
+
   public static void main(String[] args) {
     BinaryTree bt1 = new BinaryTree();
     bt1.insertRoot(1);
@@ -36,7 +65,7 @@ public class Solution {
     bt2.display(bt2.root);
 
     Solution solution = new Solution();
-    TreeNode mergedNode = solution.mergeTrees(bt1.root, bt2.root);
+    TreeNode mergedNode = solution.mergeTrees2(bt1.root, bt2.root);
 
     Stack<TreeNode> s = new Stack<>();
     TreeNode cur = mergedNode; 

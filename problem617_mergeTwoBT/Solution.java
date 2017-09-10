c52940d76787cdbe690339e28c6618e8fd47c780
Mijung Kim <MijungKimMacPro@Mijungs-MacBook-Pro-2.local>
2017-09-09 20:45:52 -0700

    simplified mergeTrees

--- problem617_mergeTwoBT/Solution.java
+++ problem617_mergeTwoBT/Solution.java
@@ -2,13 +2,18 @@ import java.util.*;
 
 public class Solution {
   public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-    if (t1 == null && t2 == null)  return null;
+    if (t1 == null) {
+      return t2;
+    }
+    if (t2 == null) {
+      return t1;
+    }
     
-    int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+    int val = t1.val + t2.val;
     TreeNode newNode = new TreeNode(val);
 
-    newNode.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
-    newNode.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+    newNode.left = mergeTrees(t1.left, t2.left);
+    newNode.right = mergeTrees(t1.right, t2.right);
     
     return newNode;
   }
@@ -56,7 +61,7 @@ class TreeNode {
 }
 
 class BinaryTree {
-  public TreeNode root;
+  TreeNode root;
   
   public BinaryTree() {
     this.root = null;

2bd99ff7e6a37e4b942ce9a8c59c5acb535c227a
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-09-11 15:58:16 -0700

    max depth using both recursive and iterative

---
+++ problem104_maxDepth/Solution.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Solution {
+  // Recursive
+  public int maxDepth2(TreeNode root) {
+    if (root == null) return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  }
+
+  // Iterative
+  public int maxDepth(TreeNode root) {
+    if (root == null) return 0;
+    
+    int depth = 0;
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    
+    while (!queue.isEmpty()) {
+      depth++;
+      
+      for (int i = 0, n = queue.size(); i < n; i++) {
+        TreeNode node = queue.poll();
+        
+        if (node.left != null)
+          queue.offer(node.left);
+        if (node.right != null)
+          queue.offer(node.right);
+      }
+    }
+    return depth;
+  }
+
+  public static void main(String[] args) {
+    TreeNode head = new TreeNode(1);
+    TreeNode newNode = new TreeNode(2);
+    head.left = newNode;
+    newNode = new TreeNode(2);
+    head.right = newNode;
+    newNode = new TreeNode(3);
+    head.left.left = newNode;
+    newNode = new TreeNode(3);
+    head.right.right = newNode;
+
+    Solution solution = new Solution();
+    int result = solution.maxDepth(head);
+    System.out.println("Result: " + result);
+
+    int result2 = solution.maxDepth2(head);
+    System.out.println("Result: " + result2);
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) { val = x; }
+}
+
+

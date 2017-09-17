cf3392b63b19a87bfad01da1cd108ddb5b66605b
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-09-17 14:28:28 -0700

    fixed a bug for the calculation of x*(x-1)/2

--- problem441_arrangeCoins/Solution.java
+++ problem441_arrangeCoins/Solution.java
@@ -9,7 +9,12 @@ public class Solution {
     while (start <= end) {
       // it effectively divides the operand by 2 and prevents overflow
       mid = (start + end) >>> 1;
-      if ((mid * (mid + 1)) / 2 <= n)
+      
+      // `1 + 2 + 3 + 4 + 5 + 6 + 7 + ... + x <= n`
+      // (x*(x+1) / 2  ==> 0.5*x*x + 0.5*x
+      double sum = 0.5 * mid * mid + 0.5 * mid;
+
+      if (sum <= n)
         start = mid + 1;
       else
         end = mid - 1;
@@ -25,7 +30,7 @@ public class Solution {
     n = 5;
     System.out.printf("arrangeCoins(%d): %d\n", n, solution.arrangeCoins(n));
 
-    n = 8;
+    n = 1804289383;
     System.out.printf("arrangeCoins(%d): %d\n", n, solution.arrangeCoins(n));
   }
 }

f9d5450226c092760f3eb4612e890bb4934620b0
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-09-17 11:43:36 -0700

    fixed the code

--- problem438_findAnagrams/Solution.java
+++ problem438_findAnagrams/Solution.java
@@ -2,43 +2,41 @@ import java.util.*;
 
 public class Solution {
   public List<Integer> findAnagrams(String s, String p) {
-    List<Integer> list = new ArrayList<>();
-    if (p.length() > s.length()) return list;
-
+    List<Integer> result = new LinkedList<>();
+    if (s.length() < p.length()) return result;
+    
     Map<Character, Integer> map = new HashMap<>();
     for (Character c : p.toCharArray()) {
-      map.put(c, map.getOrDefault(c, 0) +1);
+      map.put(c, map.getOrDefault(c, 0) + 1);
     }
-    int counter = map.size();
     
+    int counter = map.size();
     int begin = 0, end = 0;
-    int len = Integer.MAX_VALUE;
-
+    
     while (end < s.length()) {
       char c = s.charAt(end);
-
       if (map.containsKey(c)) {
-        map.put(c, map.get(c) - 1);
+        map.put(c, map.get(c)-1);
         if (map.get(c) == 0) counter--;
       }
       end++;
-
+      
       while (counter == 0) {
         char tempc = s.charAt(begin);
         if (map.containsKey(tempc)) {
           map.put(tempc, map.get(tempc) + 1);
-          if (map.get(tempc) > 0) counter++;
+          if (map.get(tempc) > 0) {
+            counter++;
+          }
         }
 
         if (end-begin == p.length()) {
-          list.add(begin);
+          result.add(begin);
         }
-
         begin++;
       }
     }
-
-    return list;
+    return result;
   }
 
   public static void main(String[] args) {
@@ -73,4 +71,5 @@ public class Solution {
     System.out.println(solution.findAnagrams(s, p));
   }
 }
+
 

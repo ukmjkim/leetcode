116583f3cf3978e1dba5d446f43ca426c245d2f9
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-09-21 15:09:03 -0700

    added a solution using recursion

--- problem572_isSubtree/Solution.java
+++ problem572_isSubtree/Solution.java
@@ -3,7 +3,22 @@ import java.util.*;
 public class Solution {
   final static int NULL = -9999;
 
-	public boolean isSubtree(TreeNode s, TreeNode t) {
+  public boolean isSubtree(TreeNode s, TreeNode t) {
+    if (s == null) return false;
+    if (isSame(s, t)) return true;
+    return isSubtree(s.left, t) || isSubtree(s.right, t);
+  }
+
+  public boolean isSame(TreeNode s, TreeNode t) {
+    if (s == null && t == null) return true;
+    if (s == null || t == null) return false;
+
+    if (s.val != t.val) return false;
+
+    return isSame(s.left, t.left) & isSame(s.right, t.right);
+  }
+
+	public boolean isSubtree2(TreeNode s, TreeNode t) {
     String sStr = bTreeToString(s);
     String tStr = bTreeToString(t);
 

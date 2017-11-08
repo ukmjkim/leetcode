450b97f6c8196a6afaf1f052301feb345e78b62b
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-11-08 12:24:36 -0800

    added test cases

--- easy/problem001_twoSum/Solution.java
+++ easy/problem001_twoSum/Solution.java
@@ -1,24 +1,55 @@
 import java.util.*;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-        for (int i=0; i < nums.length; i++) {
+public class Solution {
+  public int[] twoSum(int[] nums, int target) {
+        int[] indices = new int[2];
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
-            if (hash.containsKey(diff)) {
-                return new int[]{hash.get(diff), i};
+            if (numMap.containsKey(diff)) {
+                indices[0] = numMap.get(diff);
+                indices[1] = i;
+                return indices;
             }
-            hash.put(nums[i], i);
+            numMap.put(nums[i], i);
         }
-        
-        return new int[]{0, 0};
+        return indices;
+  }
+
+  public static void main(String[] args) {
+    int[] nums;
+  int target;
+    int[] expected;
+
+    Solution solution = new Solution();
+
+    nums = new int[] {2, 7, 11, 15};
+  target = 9;
+    expected = new int[] {0, 1};
+    if (Arrays.equals(solution.twoSum(nums, target), expected)) {
+      System.out.printf("result: matched nums: %s, target: %d, result: %s\n", Arrays.toString(nums), target, Arrays.toString(solution.twoSum(nums, target)));
+    } else {
+      System.out.printf("result: not matched nums: %s, target: %d, result: %s\n", Arrays.toString(nums), target, Arrays.toString(solution.twoSum(nums, target)));
     }
 
-    public static void main(String[] args) {
-        System.out.println("Solution Testing....");
-        int[] nums = {2, 7, 11, 15};
+    nums = new int[] {2, 7, 11, 15};
+  target = 13;
+    expected = new int[] {0, 2};
+    if (Arrays.equals(solution.twoSum(nums, target), expected)) {
+      System.out.printf("result: matched nums: %s, target: %d, result: %s\n", Arrays.toString(nums), target, Arrays.toString(solution.twoSum(nums, target)));
+    } else {
+      System.out.printf("result: not matched nums: %s, target: %d, result: %s\n", Arrays.toString(nums), target, Arrays.toString(solution.twoSum(nums, target)));
+    }  
 
-        int[] answer = new Solution().twoSum(nums, 9);
-        System.out.println("First Number: " + answer[0] + ", Second Number: " + answer[1]);
+    nums = new int[] {11, 7, 2, 15};
+  target = 18;
+    expected = new int[] {0, 1};
+    if (Arrays.equals(solution.twoSum(nums, target), expected)) {
+      System.out.printf("result: matched nums: %s, target: %d, result: %s\n", Arrays.toString(nums), target, Arrays.toString(solution.twoSum(nums, target)));
+    } else {
+      System.out.printf("result: not matched nums: %s, target: %d, result: %s\n", Arrays.toString(nums), target, Arrays.toString(solution.twoSum(nums, target)));
     }
+
+  }
 }
+
